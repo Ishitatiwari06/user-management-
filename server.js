8df1313ed6a3390b3ae6da4728ea7d7f7494a598
Ishita Tiwari <ishitatiwari060@gmail.com>
2026-02-05 10:30:42 +0530

    added log

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import app from './app.js';
+import chalk from 'chalk';
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Server is running on port 3000");
+    console.log(chalk.green("Server is running on port 3000: Success"));
 });
